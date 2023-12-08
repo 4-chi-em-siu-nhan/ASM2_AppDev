d869fbe9a1894701f3bcf94cb4e542932e0a8d50
Le Thi Thanh Huyen <lehuyen23vn@gmail.com>
2023-12-08 17:50:17 +0700

    Add project files.

---
+++ ASM2_AppDev/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASM2_AppDev.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
