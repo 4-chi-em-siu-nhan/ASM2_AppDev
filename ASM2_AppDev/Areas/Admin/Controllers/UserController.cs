773222154a8bc1c86a0883a638c7977bda53844e
Le Thi Thanh Huyen <lehuyen23vn@gmail.com>
2023-12-18 12:30:04 +0700

    Change Password

--- ASM2_AppDev/Areas/Admin/Controllers/UserController.cs
+++ ASM2_AppDev/Areas/Admin/Controllers/UserController.cs
@@ -16,10 +16,12 @@ namespace ASM2_AppDev.Areas.Admin.Controllers
     {
         private readonly ApplicationDBContext _dbContext;
         private readonly UserManager<IdentityUser> _userManager;
-        public UserController(ApplicationDBContext dBContext, UserManager<IdentityUser> userManager)
+        private readonly IPasswordHasher<IdentityUser> _passwordHash;
+        public UserController(ApplicationDBContext dBContext, UserManager<IdentityUser> userManager, IPasswordHasher<IdentityUser> passwordHash)
         {
             _dbContext = dBContext;
             _userManager = userManager;
+            _passwordHash = passwordHash;
         }
         public IActionResult Index()
         {
@@ -70,7 +72,48 @@ namespace ASM2_AppDev.Areas.Admin.Controllers
             return RedirectToAction("Index", "User");
 
         }
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+        }
 
-       
+        [HttpGet]
+        public async Task<IActionResult> ChangePassword(string id)
+        {
+            IdentityUser user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+                return View(user);
+            else
+                return RedirectToAction("Index", "User");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(string id, string password)
+        {
+            IdentityUser user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+
+                if (!string.IsNullOrEmpty(password))
+                    user.PasswordHash = _passwordHash.HashPassword(user, password);
+                else
+                    ModelState.AddModelError("", "Password cannot be empty");
+
+                if (!string.IsNullOrEmpty(password))
+                {
+                    IdentityResult result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index", "User");
+                    else
+                        Errors(result);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View(user);
+        }
+
+
     }
 }
